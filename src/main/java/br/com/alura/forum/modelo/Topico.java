c6b3a992f1295f3911a2dbef9de2e13447e3310a
Jean Amadeu <jean.amadeu2@gmail.com>
2021-02-07 10:20:21 -0300

    post

--- src/main/java/br/com/alura/forum/modelo/Topico.java
+++ src/main/java/br/com/alura/forum/modelo/Topico.java
@@ -32,8 +32,7 @@ public class Topico {
 	private List<Resposta> respostas = new ArrayList<>();
 
 	public Topico() {
-		super();
-	}
+	}	
 
 	public Topico(String titulo, String mensagem, Curso curso) {
 		this.titulo = titulo;
